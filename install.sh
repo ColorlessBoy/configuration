50b5c67fa8a9e982cc1bba70bf0644507f4db537
colorlessboy <501868024@qq.com>
2019-08-17 17:39:41 +0800

    update zsh, install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+# Install Powerline Fonts
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.local/share/fonts
+mkdir -p ~/.config/fontconfig/conf.d
+mv PowerlineSymbols.otf ~/.local/share/fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+# Install Vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install git
+sudo apt install git
+git config --global user.name "colorlessboy"
+git config --global user.email "501868024@qq.com"
+git config --global core.editor "vim"
+ssh-keygen -t rsa -b 4096 -C "501868024@qq.com"
+
+# Install Nvidia drivers
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+# ubuntu-drivers devices
+# sudo nvidia-smi
+
+# Install zsh, oh-my-zsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+
+
